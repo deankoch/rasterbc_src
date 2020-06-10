253a1f5aa8d080d9c07e4196c68dd68227161035
deankoch <deankoch@gmail.com>
2020-06-10 15:49:07 -0600

    first commit of source file scripts, with auto-generated markdown

---
+++ src_borders.R
@@ -0,0 +1,32 @@
+#' ---
+#' title: "borders"
+#' author: "Dean Koch"
+#' date: "June 10, 2020"
+#' output: github_document
+#' ---
+#'
+#' This is the initial setup script for reproducing the rasterbc dataset. It downloads some shapefiles to define BC borders, sets up config for preprocessing
+#' 
+#' The geographical projection system is set to match the one used in the `bcmaps` package (Albers NAD83, standard in BC). Resolution and extent are set to match hectaresBC.org and extents are defined for each individual output tile (10km x 10km mapsheet).
+#' 
+#' 
+#' 
+
+## R script (markdown render-ready)
+## 
+
+
+
+
+## here is a regular code comment, that will remain as such
+summary(VADeaths)
+
+#' Here's some more prose. I can use usual markdown syntax to make things
+#' **bold** or *italics*. Let's use an example from the `dotchart()` help to
+#' make a Cleveland dot plot from the `VADeaths` data. I even bother to name
+#' this chunk, so the resulting PNG has a decent name.
+#+ dotchart
+dotchart(VADeaths, main = "Death Rates in Virginia - 1940")
+
+
+# rmarkdown::render(here('src_borders.R'), run_pandoc=FALSE, clean=TRUE)
