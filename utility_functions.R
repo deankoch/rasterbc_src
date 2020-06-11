03e210b6b9defabea2201aeb13f5019ec7ee50a7
deankoch <deankoch@gmail.com>
2020-06-10 20:20:16 -0600

    wip

--- utility_functions.R
+++ utility_functions.R
@@ -6,7 +6,7 @@
 #' ---
 #' 
 #' 
-<<<<<<< HEAD
+
 #' This loads required packages, sets up parameters, and defines some helper functions. It is sourced at
 #' the beginning of all of the individual downloading/pre-processing scripts. 
 #' 
@@ -36,14 +36,18 @@ library(doSNOW)
 
 #' **important user-defined parameters**
 #' 
-#' define/create a directory for storage of all downloaded source files and output files. 
+# define/create a directory for storage of all downloaded source files and output files 
 data.dir = here('data')
 
 #' By default this is the subdirectory 'data' relative to the location of the R project file (ie. .../rasterbc/data). 
-#' Around 60GB of data in total will be downloaded/written. Feel free to change this (eg. to a drive with bigger storage), 
-#' but be careful not to assign it to to an existing directory as I do not check for existing files, so *anything already in 
+#' Around 60GB of data in total will be downloaded/written. Feel free to change this to another path (eg. a drive with more free
+#' space), but be careful not to assign it to to an existing directory as I do not check for existing files, so *anything already in 
 #' data.dir could get overwritten*.
 #'
+
+#  set the number of cores to use with doSNOW (set to 1 if doSNOW/multicore not available)
+n.cores = 3
+#' Rasterization requires around 6GB per core, so consider reducing this if you are encountering out of memory errors 
 
 #+ echo=FALSE
 # Convert to markdown by running the following line (uncommented)...
