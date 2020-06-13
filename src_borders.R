9235c9e0ea3e99cb6e08bd56eb26b55ab2b947fd
deankoch <deankoch@gmail.com>
2020-06-13 15:01:44 -0600

    finishing for the day. Probably 2 hours work left on src_borders and utility_functions

--- src_borders.R
+++ src_borders.R
@@ -96,17 +96,17 @@ if(!all(file.exists(cfg$src$fname)) | force.download)
 #' 
 #' 
 
+#' First, prepare a mask for in-province pixels and save it to disk:
+
 # load the provincial boundaries polygons and NTS/SNRC blocks, reprojecting to crs(bc.mask.tif)
 prov.sf = sf::st_transform(sf::st_read(dsn=cfg$src$fname['prov']), MPB_crs()$epsg) 
 snrc.sf = sf::st_transform(sf::st_read(dsn=cfg$src$fname['snrc']), MPB_crs()$epsg)
-
-#' First, prepare a mask for in-province pixels and save it to disk:
 
 # create a mask for in-province pixels (setting reference extent and resolution)
 ref.tif = raster::crop(MPB_crs()$tif, prov.sf[prov.sf$PROV_TERRI=='BC',1], snap='out')
 bc.sf = prov.sf[prov.sf$PROV_TERRI=='BC',1]
 
-# replace character column with numeric, rasterize, and write 
+# replace character column with numeric, rasterize, and write (20.2 MB) 
 #+ eval=FALSE
 bc.sf$PROV_TERRI = 0
 bc.mask.tif = fasterize::fasterize(sf=bc.sf, raster=ref.tif, field='PROV_TERRI', fun='any')
@@ -142,15 +142,28 @@ snrc.sf = sf::st_read(cfg$out$path$shp['snrc'])
 prov.sf = sf::st_read(cfg$out$path$shp['prov'])
 cfg$out$code = snrc.sf$NTS_SN
 
+#' Create `latitude`, `longitude` layers via `sp` package (slow step, taking around 5 minutes), then save to disk (285 MB, 352 MB)
+#+ eval=FALSE
+# load the in-province mask (raster), convert to points dataframe 
+bc.mask.tif = raster::raster(cfg$out$path$tif$full['prov'])
+bc.coords.df = data.frame(raster::coordinates(bc.mask.tif))
+sp::coordinates(bc.coords.df) = c('x', 'y')
+sp::proj4string(bc.coords.df) = raster::crs(bc.mask.tif)
 
-#
+# reproject to lat/long, apply mask, and write
+bc.coords.df = sp::spTransform(bc.coords.df, CRS=sp::CRS('+proj=longlat +ellps=GRS80'))
+raster::writeRaster(raster::mask(raster::setValues(bc.mask.tif, bc.coords.df$y), bc.mask.tif), cfg$out$path$tif$full['latitude'], overwrite=TRUE)
+raster::writeRaster(raster::mask(raster::setValues(bc.mask.tif, bc.coords.df$x), bc.mask.tif), cfg$out$path$tif$full['longitude'], overwrite=TRUE)
 
+#' Finally, split these layers up into mapsheets corresponding to the NTS/SNRC codes
+# To do: add block subfolder to data/*/ in utility_functions.R
+# To do: add the output filenames earlier in this script
+# To do: build a loop that does the crop/save. Remember to reload rasters where eval=FALSE
+# To do: write RData file with final cfg list
+# par(mfrow)
+# plot(raster(cfg$out$path$tif$full['latitude']))
+# plot(raster(cfg$out$path$tif$full['longitude']))
 
-# convert relevant character-type features to numeric, store coding
-# convert relevant character-type features to numeric, store coding
-
-
-#bc.mask.tif = fasterize::fasterize(prov.sf[prov.sf$PROV_TERRI=='BC',1], ref.pine.tif, 'PROV_TERRI', 'any')
 
 #+ include=FALSE
 # Convert to markdown by running the following line (uncommented)...
